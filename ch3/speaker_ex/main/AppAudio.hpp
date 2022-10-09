a5197c491de21541618bd302e13f15487de81911
ozanoner <ozan.oner@gmail.com>
2022-10-09 16:25:15 +0100

    app audio with play

--- ch3/speaker_ex/main/AppAudio.hpp
+++ ch3/speaker_ex/main/AppAudio.hpp
@@ -18,6 +18,7 @@ namespace app
     private:
         AppSettings &m_settings;
         bool m_playing;
+        file_iterator_instance_t *file_iterator;
 
     public:
         AppAudio(AppSettings &settings) : m_settings(settings), m_playing(false) {}
@@ -31,6 +32,8 @@ namespace app
                                             .mute_fn = fn,
                                             .priority = 1};
             ESP_ERROR_CHECK(audio_player_new(config));
+
+            file_iterator = file_iterator_new("/spiffs/mp3");
         }
 
         void mute(bool m)
@@ -41,6 +44,58 @@ namespace app
                 bsp_codec_set_voice_volume(m_settings.getVolume());
             }
             ESP_LOGI(__func__, "%s, volume:%d", m ? "muted" : "unmuted", m_settings.getVolume());
+        }
+
+        void play(void)
+        {
+            ESP_LOGI(__func__, "button play");
+            static bool playing = false;
+            if (!playing)
+            {
+                char filename[128];
+                file_iterator_get_full_path_from_index(file_iterator, file_iterator_get_index(file_iterator), filename, sizeof(filename));
+                FILE *fp = fopen(filename, "rb");
+                if (!fp)
+                {
+                    ESP_LOGE(__func__, "unable to open '%s'", filename);
+                    return;
+                }
+                else
+                {
+                    ESP_LOGI(__func__, "playing %s", filename);
+                }
+                audio_player_play(fp);
+            }
+            else
+            {
+                audio_player_pause();
+            }
+
+            playing = !playing;
+        }
+
+        void volume_up(void)
+        {
+            uint8_t volume = m_settings.getVolume();
+            if (volume < 100)
+            {
+                volume += 10;
+                ESP_LOGI(__func__, "volume up (%d)", volume);
+                bsp_codec_set_voice_volume(volume);
+                m_settings.updateVolume(volume);
+            }
+        }
+
+        void volume_down(void)
+        {
+            uint8_t volume = m_settings.getVolume();
+            if (volume > 0)
+            {
+                volume -= 10;
+                ESP_LOGI(__func__, "volume down (%d)", volume);
+                bsp_codec_set_voice_volume(volume);
+                m_settings.updateVolume(volume);
+            }
         }
     };
 }
--- ch3/speaker_ex/main/main.cpp
+++ ch3/speaker_ex/main/main.cpp
@@ -1,31 +1,6 @@
-/*
- * SPDX-FileCopyrightText: 2015-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-
-#include <stdio.h>
-#include "esp_heap_caps.h"
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "esp_check.h"
-// #include "nvs_flash.h"
-// #include "nvs.h"
-#include "bsp_codec.h"
-#include "bsp_board.h"
-#include "bsp_btn.h"
-#include "bsp_storage.h"
-#include "audio_player.h"
-#include "file_iterator.h"
-
 #include "AppSettings.hpp"
 #include "AppAudio.hpp"
 #include "AppButton.hpp"
-
-static const char *TAG = "main";
-file_iterator_instance_t *file_iterator;
 
 namespace
 {
@@ -41,69 +16,23 @@ namespace
 
     void play_music(void *data)
     {
-        ESP_LOGI(__func__, "button play");
-        static bool playing = false;
-        if (!playing)
-        {
-            char filename[128];
-            file_iterator_get_full_path_from_index(file_iterator, file_iterator_get_index(file_iterator), filename, sizeof(filename));
-            FILE *fp = fopen(filename, "rb");
-            if (!fp)
-            {
-                ESP_LOGE(TAG, "unable to open '%s'", filename);
-                return;
-            }
-            else
-            {
-                ESP_LOGI(__func__, "playing %s", filename);
-            }
-            audio_player_play(fp);
-        }
-        else
-        {
-            audio_player_pause();
-        }
-
-        playing = !playing;
+        m_app_audio.play();
     }
 
     void volume_up(void *data)
     {
-        uint8_t volume = m_app_settings.getVolume();
-        if (volume < 100)
-        {
-            volume += 10;
-            ESP_LOGI(__func__, "volume up (%d)", volume);
-            bsp_codec_set_voice_volume(volume);
-            m_app_settings.updateVolume(volume);
-        }
+        m_app_audio.volume_up();
     }
 
     void volume_down(void *data)
     {
-        uint8_t volume = m_app_settings.getVolume();
-        if (volume > 0)
-        {
-            volume -= 10;
-            ESP_LOGI(__func__, "volume down (%d)", volume);
-            bsp_codec_set_voice_volume(volume);
-            m_app_settings.updateVolume(volume);
-        }
+        m_app_audio.volume_down();
     }
 }
 
 extern "C" void app_main(void)
 {
-    ESP_LOGI(TAG, "Compile time: %s %s", __DATE__, __TIME__);
-
     m_app_settings.init();
     m_app_audio.init(audio_mute_function);
-
-    file_iterator = file_iterator_new("/spiffs/mp3");
-    assert(file_iterator != NULL);
-
     m_app_btn.init(play_music, volume_down, volume_up);
-    // bsp_btn_register_callback(BOARD_BTN_ID_PREV, BUTTON_PRESS_DOWN, play_music, NULL);
-    // bsp_btn_register_callback(BOARD_BTN_ID_ENTER, BUTTON_PRESS_DOWN, volume_down, NULL);
-    // bsp_btn_register_callback(BOARD_BTN_ID_NEXT, BUTTON_PRESS_DOWN, volume_up, NULL);
 }

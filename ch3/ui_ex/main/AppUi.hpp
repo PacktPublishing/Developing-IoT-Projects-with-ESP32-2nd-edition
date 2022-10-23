4e86a0f9c4a015d8127afe5861a844ee3e060e0f
ozanoner <ozan.oner@gmail.com>
2022-10-23 13:32:49 +0100

    ui_ex refactored

--- ch3/ui_ex/main/AppUi.hpp
+++ ch3/ui_ex/main/AppUi.hpp
@@ -3,15 +3,12 @@
 #include <sys/time.h>
 #include <mutex>
 
+#include "bsp_lcd.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "lvgl/lvgl.h"
 #include "lv_port/lv_port.h"
-
-namespace
-{
-}
 
 namespace app
 {
@@ -20,7 +17,7 @@ namespace app
     public:
         void init(void)
         {
-            ESP_ERROR_CHECK(lv_port_init());
+            lv_port_init();
 
             m_label_title = lv_label_create(lv_scr_act());
             lv_obj_set_style_text_color(m_label_title, lv_color_make(0, 0, 0), LV_STATE_DEFAULT);
@@ -28,8 +25,9 @@ namespace app
             lv_label_set_text(m_label_title, "Press a button");
             lv_obj_align(m_label_title, LV_ALIGN_TOP_MID, 0, 100);
 
-            BaseType_t ret_val = xTaskCreatePinnedToCore(lvgl_task, "lvgl", 6 * 1024, nullptr, configMAX_PRIORITIES - 3, nullptr, 0);
-            ESP_ERROR_CHECK((pdPASS == ret_val) ? ESP_OK : ESP_FAIL);
+            xTaskCreatePinnedToCore(lvgl_task, "lvgl", 6 * 1024, nullptr, configMAX_PRIORITIES - 3, nullptr, 0);
+         
+            bsp_lcd_set_backlight(true); // Turn on the backlight after gui initialize
         }
 
         void setLabelText(const char *lbl_txt)
--- ch3/ui_ex/main/main.cpp
+++ ch3/ui_ex/main/main.cpp
@@ -1,13 +1,11 @@
-#include "esp_log.h"
 #include "bsp_board.h"
-#include "bsp_lcd.h"
 
 #include "AppButton.hpp"
 #include "AppUi.hpp"
 
-#define APPBTN_LEFT_TEXT "BTN_LEFT"
-#define APPBTN_MIDDLE_TEXT "BTN_MIDDLE"
-#define APPBTN_RIGHT_TEXT "BTN_RIGHT"
+#define APPBTN_MIDDLE_TEXT "Middle button"
+#define APPBTN_LEFT_TEXT "Left button"
+#define APPBTN_RIGHT_TEXT "Right button"
 
 namespace
 {
@@ -23,36 +21,48 @@ extern "C" void app_main(void)
 {
     bsp_board_init();
 
-    auto btn_down = [](void *btn_ptr)
+    auto btn_down_handler = [](void *btn_ptr)
     {
         app::AppButton &btn = app::AppButton::getObject(btn_ptr);
         switch (btn.getType())
         {
         case APPBTN_MIDDLE:
-            m_app_ui.setLabelText(APPBTN_MIDDLE_TEXT " pressed");
+            m_app_ui.setLabelText(APPBTN_MIDDLE_TEXT " down");
             break;
         case APPBTN_LEFT:
-            m_app_ui.setLabelText(APPBTN_LEFT_TEXT " pressed");
+            m_app_ui.setLabelText(APPBTN_LEFT_TEXT " down");
             break;
         case APPBTN_RIGHT:
-            m_app_ui.setLabelText(APPBTN_RIGHT_TEXT " pressed");
+            m_app_ui.setLabelText(APPBTN_RIGHT_TEXT " down");
             break;
 
         default:
             break;
         }
-        ESP_LOGI(TAG, "%d down", btn.getType());
     };
 
-    auto btn_up = [](void *btn_ptr)
+    auto btn_up_handler = [](void *btn_ptr)
     {
         app::AppButton &btn = app::AppButton::getObject(btn_ptr);
-        ESP_LOGI(TAG, "%d up", btn.getType());
+        switch (btn.getType())
+        {
+        case APPBTN_MIDDLE:
+            m_app_ui.setLabelText(APPBTN_MIDDLE_TEXT " up");
+            break;
+        case APPBTN_LEFT:
+            m_app_ui.setLabelText(APPBTN_LEFT_TEXT " up");
+            break;
+        case APPBTN_RIGHT:
+            m_app_ui.setLabelText(APPBTN_RIGHT_TEXT " up");
+            break;
+
+        default:
+            break;
+        }
     };
 
-    m_btn_middle.init(btn_down, btn_up);
-    m_btn_left.init(btn_down, btn_up);
-    m_btn_right.init(btn_down, btn_up);
+    m_btn_middle.init(btn_down_handler, btn_up_handler);
+    m_btn_left.init(btn_down_handler, btn_up_handler);
+    m_btn_right.init(btn_down_handler, btn_up_handler);
     m_app_ui.init();
-    bsp_lcd_set_backlight(true); // Turn on the backlight after gui initialize
 }

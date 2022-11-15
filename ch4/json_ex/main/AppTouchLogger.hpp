bf0b44548f16e3d17d2640fb3b0c03544d22009d
ozanoner <ozan.oner@gmail.com>
2022-11-15 20:44:04 +0000

    json_ex refactored

--- ch4/json_ex/main/AppTouchLogger.hpp
+++ ch4/json_ex/main/AppTouchLogger.hpp
@@ -4,6 +4,8 @@
 #include <ctime>
 #include <vector>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_log.h"
 #include "driver/touch_pad.h"
 #include "json.hpp"
@@ -14,10 +16,9 @@ namespace app
     {
         uint32_t timestamp;
         uint32_t pad_num;
-        uint32_t pad_status;
         uint32_t intr_mask;
     };
-    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(AppTouch_t, timestamp, pad_num, pad_status, intr_mask);
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(AppTouch_t, timestamp, pad_num, intr_mask);
 
     class AppTouchLogger
     {
@@ -27,18 +28,8 @@ namespace app
 
         static void touchsensor_interrupt_cb(void *arg)
         {
-            // uint32_t pad_num = touch_pad_get_current_meas_channel();
-            // uint32_t pad_status = touch_pad_get_status();
-            // uint32_t intr_mask = touch_pad_read_intr_status_mask();
-            // if (intr_mask & TOUCH_PAD_INTR_MASK_ACTIVE)
-            // {
-            // }
-            // else
-            // {
-            // }
             AppTouch_t app_touch{esp_log_timestamp(),
                                  touch_pad_get_current_meas_channel(),
-                                 touch_pad_get_status(),
                                  touch_pad_read_intr_status_mask()};
 
             AppTouchLogger &obj = *(reinterpret_cast<AppTouchLogger *>(arg));
@@ -50,8 +41,28 @@ namespace app
         {
             touch_pad_init();
             touch_pad_config(TOUCH_PAD_NUM9);
+
+            touch_filter_config_t filter_info = {
+                .mode = TOUCH_PAD_FILTER_IIR_16,
+                .debounce_cnt = 1,
+                .noise_thr = 0,
+                .jitter_step = 4,
+                .smh_lvl = TOUCH_PAD_SMOOTH_IIR_2,
+            };
+            touch_pad_filter_set_config(&filter_info);
+            touch_pad_filter_enable();
+            touch_pad_timeout_set(true, SOC_TOUCH_PAD_THRESHOLD_MAX);
+
             touch_pad_isr_register(touchsensor_interrupt_cb, this, TOUCH_PAD_INTR_MASK_ALL);
             touch_pad_intr_enable(TOUCH_PAD_INTR_MASK_ACTIVE | TOUCH_PAD_INTR_MASK_INACTIVE);
+
+            touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
+            touch_pad_fsm_start();
+
+            vTaskDelay(pdMS_TO_TICKS(50));
+            uint32_t touch_value;
+            touch_pad_read_benchmark(TOUCH_PAD_NUM9, &touch_value);
+            touch_pad_set_thresh(TOUCH_PAD_NUM9, touch_value * .2);
         }
 
         nlohmann::json serialize(void)

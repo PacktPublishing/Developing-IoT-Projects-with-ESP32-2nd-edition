07df54be22a43f7fdbf1afc37a220d0ff48b5db3
Ozan Oner <ozan.oner@gmail.com>
2024-06-21 21:53:44 +0100

    json ex updated

--- ch4/json_ex/main/AppTouchLogger.hpp
+++ ch4/json_ex/main/AppTouchLogger.hpp
@@ -53,8 +53,8 @@ namespace app
             touch_pad_filter_enable();
             touch_pad_timeout_set(true, SOC_TOUCH_PAD_THRESHOLD_MAX);
 
-            touch_pad_isr_register(touchsensor_interrupt_cb, this, TOUCH_PAD_INTR_MASK_ALL);
-            touch_pad_intr_enable(TOUCH_PAD_INTR_MASK_ACTIVE | TOUCH_PAD_INTR_MASK_INACTIVE);
+            touch_pad_isr_register(touchsensor_interrupt_cb, this, static_cast<touch_pad_intr_mask_t>(TOUCH_PAD_INTR_MASK_ALL));
+            touch_pad_intr_enable(static_cast<touch_pad_intr_mask_t>(TOUCH_PAD_INTR_MASK_ACTIVE | TOUCH_PAD_INTR_MASK_INACTIVE));
 
             touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
             touch_pad_fsm_start();

54fd6f3168b4496fc5dc4ba59c85b846b8286bf4
Ozan Oner <ozan.oner@gmail.com>
2024-06-21 21:59:04 +0100

    miniz ex updated

--- ch4/miniz_ex/main/AppButton.hpp
+++ ch4/miniz_ex/main/AppButton.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
 #include "bsp_board.h"
-#include "bsp_btn.h"
 
 namespace app
 {
-    using btn_pressed_handler_f = void (*)(void *);
-
     class AppButton
     {
     public:
-        void init(btn_pressed_handler_f l, btn_pressed_handler_f m)
+        void init(button_cb_t l, button_cb_t m)
         {
+            bsp_i2c_init();
             bsp_board_init();
             bsp_btn_register_callback(BOARD_BTN_ID_PREV, BUTTON_PRESS_DOWN, l, nullptr);
             bsp_btn_register_callback(BOARD_BTN_ID_ENTER, BUTTON_PRESS_DOWN, m, nullptr);

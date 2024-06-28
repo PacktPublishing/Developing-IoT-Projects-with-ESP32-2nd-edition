e669b5ada11ebe6782b3ab6b3bdab3805e700ce4
Ozan Oner <ozan.oner@gmail.com>
2024-06-28 21:13:39 +0100

    tflite-ex updated

--- ch10/tflite_ex/main/AppUi.hpp
+++ ch10/tflite_ex/main/AppUi.hpp
@@ -9,9 +9,7 @@
 #include "esp_log.h"
 
 #include "bsp_board.h"
-#include "bsp_lcd.h"
-#include "lvgl/lvgl.h"
-#include "lv_port/lv_port.h"
+#include "lvgl.h"
 #include "ui.h"
 
 namespace app
@@ -20,17 +18,11 @@ namespace app
     {
     private:
         std::mutex m_ui_access;
-        static void lvglTask(void *arg);
 
     public:
         void init(void)
         {
-            bsp_board_init();
-            lv_port_init();
             ui_init();
-            xTaskCreate(lvglTask, "lvgl", 6 * 1024, this, 3, nullptr);
-
-            bsp_lcd_set_backlight(true);
         }
 
         void drawSinePoint(float x_val, float y_val)
@@ -43,18 +35,4 @@ namespace app
             ui_setPoint(x_coord, y_coord);
         }
     };
-
-    void AppUi::lvglTask(void *arg)
-    {
-        AppUi *obj = reinterpret_cast<AppUi *>(arg);
-
-        while (true)
-        {
-            {
-                std::lock_guard<std::mutex> lock(obj->m_ui_access);
-                lv_task_handler();
-            }
-            vTaskDelay(pdMS_TO_TICKS(10));
-        }
-    }
 }
